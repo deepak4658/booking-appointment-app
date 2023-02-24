0468d5a15fbbc03f3ea8337f2c0c6c22f8d00ccf
Deepak Kumar <deepakgzsccet@hotmail.com>
2023-02-24 14:12:51 +0530

    Task10Final Booking App

---
+++ validation.js
@@ -0,0 +1,13 @@
+function onsignup(){
+    var inputName= document.getElementById("name");
+    var inputEmail= document.getElementById("email");
+    var inputPhone= document.getElementById("phone");
+    var inputTime= document.getElementById("time");
+    var inputDate= document.getElementById("date");
+    localStorage.setItem("name", inputName.value);
+    localStorage.setItem("email", inputEmail.value);
+    localStorage.setItem("phone", inputPhone.value);
+    localStorage.setItem("time", inputTime.value);
+    localStorage.setItem("date", inputDate.value);
+
+   }
