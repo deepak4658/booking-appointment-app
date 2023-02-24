06ec87dfb6dcbd91bf481e4cb8b1482e6328448f
Deepak Kumar <deepakgzsccet@hotmail.com>
2023-02-24 16:46:29 +0530

    Task11Final

--- validation.js
+++ validation.js
@@ -1,15 +1,33 @@
 function onsignup(){
-    var inputName= document.getElementById("name");
-    var inputEmail= document.getElementById("email");
-    var inputPhone= document.getElementById("phone");
-    var inputTime= document.getElementById("time");
-    var inputDate= document.getElementById("date");
-    localStorage.setItem("name", inputName.value);
-    localStorage.setItem("email", inputEmail.value);
-    localStorage.setItem("phone", inputPhone.value);
-    localStorage.setItem("time", inputTime.value);
-    localStorage.setItem("date", inputDate.value);
+
+    let name, email, phone, time, date;
+    name= document.getElementById("name").value;
+email= document.getElementById("email").value;
+phone=  document.getElementById("phone").value;
+time= document.getElementById("time").value;
+date= document.getElementById("date").value;
+
+let user= new Array();
+user= JSON.parse(localStorage.getItem("user"))?JSON.parse(localStorage.getItem("user")):[]
+if(user.some((v)=>{return v.email==email}))
+{
+    alert("Already registerd");
+
+}
+else
+{
+    user.push({
+        "name":name,
+        "email":email,
+        "phone": phone,
+        "time": time,
+        "date": date
+    })
+    localStorage.setItem("user", JSON.stringify(user));
+}
 
 
-    localStorage.setItem('name', JSON.stringify(onsignup ));
    }
+
+  
+
